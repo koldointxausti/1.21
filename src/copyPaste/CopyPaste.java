5cdadcebaee6f03936af23027131a0cd99319b9e
koldointxaustiusername <ik013043z1@H013043.NET>
2018-10-30 13:59:55 +0100

    v2

--- src/copyPaste/CopyPaste.java
+++ src/copyPaste/CopyPaste.java
@@ -1,14 +1,29 @@
 package copyPaste;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+ 
 public class CopyPaste {
-	public static void main(String[] args) {
-		try {
-				FileInputStream out = new FileInputStream("originFileToCopy.txt");
-				FileOutputStream in = new FileOutputStream("estinyFileToCopy.txt");		
-		}catch (FileNotFoundException e){
-			System.out.println("File not found");
-		}
-	}
+   public static void main(String[] args) {
+      FileInputStream ins = null;
+      FileOutputStream outs = null;
+      try {
+         File copyFrom =new File("originFileToCopy.txt");
+         File copyTo =new File("destinyFileToCopy.txt");
+         ins = new FileInputStream(copyFrom);
+         outs = new FileOutputStream(copyTo);
+         byte[] buffer = new byte[1024];
+         int length;
+         
+         while ((length = ins.read(buffer)) > 0) {
+            outs.write(buffer, 0, length);
+         } 
+         ins.close();
+         outs.close();
+         System.out.println("File copied successfully!!");
+      } catch(IOException ioe) {
+         ioe.printStackTrace();
+      } 
+   }
 }
